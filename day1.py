50700414f527b080397b08d5dc0c0b2b0340e053
Clement TURMEL <clemturmel@gmail.com>
2023-12-07 14:19:21 +0100

    day 1 part 1 completed !

--- day1.py
+++ day1.py
@@ -1,6 +1,6 @@
 import pytest
 from markdown_writter import *
-
+import re
 #python -m pytest day1.py
 
 @pytest.fixture(scope="module")
@@ -11,13 +11,39 @@ def doc():
 
 
 @pytest.mark.parametrize("line, expected_value", [
-    ("12", 12),
-    ("36", 36),
-    ("4abcd8", 48)
+    ("12",      12),
+    ("36",      36),
+    ("4abcd8",  48),
+    ("a57b",    57),
+    ("1b2c3",   13),
+    ("""a6g7fce
+     tisnro4kfe2
+     frf8jrej9rt""", 67+42+89),
+    ("aaaa7bbbb", 77),
+    ("""1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet""", 142)
 ])
-def test_calibrate_should_return_the_first_digit_and_the_last_digit_of_a_line(doc, line, expected_value):
+def test_calibrate_should_return_the_addition_of_numbers_created_from_first_and_last_digit_of_each_line(doc, line, expected_value):
     result = calibrate(line)
+    doc.log(f"given lines ```{line}```, calibrate return ```{result}```")
     assert result == expected_value
 
-def calibrate(line):
-    return int(f"{line[0]}{line[len(line)-1]}")
+
+def test_calibrate_with_file_input(doc):
+    text_input = ""
+    with open("day_1_input.txt", "r") as file_input:
+        text_input = file_input.read()
+    
+    result = calibrate(text_input)
+    assert result == 55834
+
+
+def calibrate(lines):
+    sum = 0
+    for line in lines.splitlines():
+        numbers = re.findall(r'[0-9]', line)
+        sum += int(f"{numbers[0]}{numbers[len(numbers)-1]}")
+
+    return sum
---
+++ documentation/day1.md
@@ -0,0 +1,21 @@
+given lines ```12```, calibrate return ```12```
+
+given lines ```36```, calibrate return ```36```
+
+given lines ```4abcd8```, calibrate return ```48```
+
+given lines ```a57b```, calibrate return ```57```
+
+given lines ```1b2c3```, calibrate return ```13```
+
+given lines ```a6g7fce
+     tisnro4kfe2
+     frf8jrej9rt```, calibrate return ```198```
+
+given lines ```aaaa7bbbb```, calibrate return ```77```
+
+given lines ```1abc2
+    pqr3stu8vwx
+    a1b2c3d4e5f
+    treb7uchet```, calibrate return ```142```
+
